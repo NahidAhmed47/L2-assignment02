c71698270d27ef36b4b3eac0999ef6080b56c653
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-11-23 14:45:49 +0600

    user interface and model done

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,27 @@
+export type TOrder = {
+    productName: string,
+    price: number,
+    quantity: number,
+}
+
+export type TAddress = {
+    street: string,
+    city: string,
+    country: string
+}
+
+export type TUser = {
+    userId: number,
+    username: string,
+    password: string,
+    fullName: {
+        firstName: string,
+        lastName: string
+    },
+    age: number,
+    email: string,
+    isActive: boolean,
+    hobbies: string[],
+    address: TAddress,
+    orders?: TOrder[],
+}
